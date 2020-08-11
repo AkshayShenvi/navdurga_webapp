9b913287ad1857e28c635763c3c0ad0467680364
Akshay Shenvi <akshayshenvi@gmail.com>
2020-08-11 17:36:24 -0500

    Created new branch for users and added model

--- navdurga_backend/models/users.js
+++ navdurga_backend/models/users.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const patientSchema = new Schema({
+const userSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   last_used: { type: Date, required: true },
 });
 
-module.exports = mongoose.model("patients", patientSchema);
+module.exports = mongoose.model("users", userSchema);
